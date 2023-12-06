372b4d149ef79d2ca8d17e7907699b9682cb769e
kimmangyu <vlsual0917@gmail.com>
2023-12-06 21:52:13 +0900

    초기세팅 완료

---
+++ src/main/java/coduck/igochaja/IgochajaApplication.java
@@ -0,0 +1,13 @@
+package coduck.igochaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgochajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgochajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coduck/igochaja/IgochajaApplicationTests.java
@@ -0,0 +1,13 @@
+package coduck.igochaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgochajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
