5c00cd4e14ce18ae7936e4bf0b50641cc34d5f22
yeonsii <37923273+yeonsii@users.noreply.github.com>
2023-12-19 22:10:35 +0900

    Revert "Edit"

--- src/main/java/coduck/igochaja/Repository/UserRepository.java
+++ src/main/java/coduck/igochaja/Repository/UserRepository.java
@@ -1,14 +1,25 @@
 package coduck.igochaja.Repository;
 
+
 import coduck.igochaja.Model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
 
 public interface UserRepository extends MongoRepository<User, String> {
 
     default User saveUser(String socialId, String name, String email, String social, String image) {
-        User newUser = new User(socialId, name, email, social, image);
-        return save(newUser);
+        return save(new User(socialId, name, email, social, image));
     }
 
-    User findByEmail(String email);
+    public User findByEmail(String email, String social);
+
+    default User ProfileImage(String fileUrl, String objectId) {
+        return findById(objectId)
+                .map(user -> {
+                    user.setImage(fileUrl);
+                    return save(user);
+                })
+                .orElse(null);
+    }
 }
