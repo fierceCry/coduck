751bdb464b85da7d1c7c133022b6409c10b8fd72
kimmangyu <vlsual0917@gmail.com>
2023-12-20 18:50:48 +0900

    postgresql 마이그레션 완료

---
+++ src/main/resources/db/migration/V5__Create_Post_Likes_Table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE post_likes (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    post_id INT NOT NULL REFERENCES posts(id),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
