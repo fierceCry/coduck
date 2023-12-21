f10792abcfec23fa17c4ef0bce82adc11f040887
KCH <130543414+kkkkcccchhhh@users.noreply.github.com>
2023-12-21 21:01:26 +0900

    google login 완성

--- src/main/java/coduck/igochaja/Controller/GoogleController.java
+++ src/main/java/coduck/igochaja/Controller/GoogleController.java
@@ -5,10 +5,13 @@ import coduck.igochaja.Model.User;
 import coduck.igochaja.Service.GoogleService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Map;
 
 @Controller
 @RestController
@@ -21,9 +24,12 @@ public class GoogleController {
         this.googleService = googleService;
     }
     @GetMapping("/code")
-    public void googleLogin(@RequestParam String code) {
-        googleService.socialLogin(code);
-
+    public ResponseEntity<Map<String, Object>> googleLogin(@RequestParam String code) {
+        try {
+        Map<String, Object> userInfo = googleService.socialLogin(code);
+            return new ResponseEntity<>(userInfo, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
-
 }
--- src/main/java/coduck/igochaja/Service/GoogleService.java
+++ src/main/java/coduck/igochaja/Service/GoogleService.java
@@ -3,6 +3,7 @@ package coduck.igochaja.Service;
 import coduck.igochaja.Config.GoogleConfig;
 import coduck.igochaja.Config.JwtTokenConfig;
 import coduck.igochaja.Repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
@@ -10,6 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpStatus;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class GoogleService {
@@ -21,28 +26,20 @@ public class GoogleService {
     private JwtTokenConfig jwtTokenConfig;
 
     @Autowired
-    public GoogleService(GoogleConfig googleConfig, UserRepository userRepository, JwtTokenConfig jwtTokenConfig)
-    {
+    public GoogleService(GoogleConfig googleConfig, UserRepository userRepository, JwtTokenConfig jwtTokenConfig) {
         this.userRepository = userRepository;
         this.googleConfig = googleConfig;
         this.jwtTokenConfig = jwtTokenConfig;
     }
-    public void socialLogin(String code) {
+
+    public Map<String, Object> socialLogin(String code) {
         String accessToken = getAccessToken(code);
-        JsonNode userResourceNode = getUserInfo(accessToken);
-        System.out.println("userResourceNode = " + userResourceNode);
+        JsonNode userInfo = getUserInfo(accessToken);
 
-        String id = userResourceNode.get("id").asText();
-        String email = userResourceNode.get("email").asText();
-        String nickname = userResourceNode.get("name").asText();
-        System.out.println("id = " + id);
-        System.out.println("email = " + email);
-        System.out.println("nickname = " + nickname);
-
+        return processUserInfo(userInfo);
     }
 
     private String getAccessToken(String authorizationCode) {
-
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("code", authorizationCode);
         params.add("client_id", googleConfig.getClientId());
@@ -59,14 +56,48 @@ public class GoogleService {
         JsonNode accessTokenNode = responseNode.getBody();
         return accessTokenNode.get("access_token").asText();
     }
-    private JsonNode getUserInfo(String accessToken) {
 
+    private JsonNode getUserInfo(String accessToken) {
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Bearer " + accessToken);
         HttpEntity entity = new HttpEntity(headers);
         return restTemplate.exchange(googleConfig.getResourceUrl(), HttpMethod.GET, entity, JsonNode.class).getBody();
     }
 
+    private Map<String, Object> processUserInfo(JsonNode userInfo) {
+        try {
+            String socialId = userInfo.get("id").asText();
+            String name = userInfo.get("name").asText();
+            String email = userInfo.get("email").asText();
+            String image = userInfo.path("profile_image").asText();
+            String social = "GOOGLE";
 
+            Map<String, Object> existingUser = userRepository.findUserByEmailAndSocial(email, social);
+
+            if (existingUser != null) {
+                return generateTokenMap(existingUser);
+            } else {
+                // 이 부분에서 UserRepository의 saveUser 메서드를 사용하여 유저를 저장
+                Map<String, Object> savedUser = userRepository.saveUser(socialId, name, email, social, image);
+
+                if (savedUser != null && !savedUser.isEmpty()) {
+                    return generateTokenMap(savedUser);
+                } else {
+                    return Map.of("message", "user registration failed", "status", HttpStatus.BAD_REQUEST.value());
+                }
+            }
+        } catch (Exception e) {
+            return Map.of("message", "JSON parsing failed", "status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
+    }
+
+    private Map<String, Object> generateTokenMap(Map<String, Object> result) {
+        String token = jwtTokenConfig.generateToken(result);
+        Map<String, Object> tokenMap = new HashMap<>();
+        tokenMap.put("token", token);
+        tokenMap.put("name", result.get("name"));
+        tokenMap.put("email", result.get("email"));
+        tokenMap.put("image", result.get("image"));
+        return tokenMap;
+    }
 }
-
